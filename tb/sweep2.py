af07a8da38feaa1b52f682fbacbdaa27aae000a0
Brian Crafton <bcrafton3@gatech.edu>
2023-04-23 14:51:59 -0400

    sweeps

--- tb/sweep2.py
+++ tb/sweep2.py
@@ -7,7 +7,6 @@ from chip1 import *
 
 board = Board()
 board.init()
-board.set_voltage('avdd_wl', 400)
 
 chip = Chip1()
 chip.rst()
@@ -15,24 +14,25 @@ chip.rst()
 chip.write_reg(reg=9, addr=5, val=0x01)
 
 for i in range(16 + 1):
-    chip.write_cam(0xa, i, 0x0f)
+    chip.write_cam(0xa, i, 0x1)
 
-for vb1 in [250]:
+for vb1 in [300]:
     board.set_voltage('vb1', vb1)
-    for vb0 in [200]:
+    for vb0 in [250]:
         board.set_voltage('vb0', vb0)
-        for vref in range(50, 500, 25):
+        for vref in range(50, 350, 5):
             board.set_voltage('vref', vref)
             print (vb1, vb0, vref, end=' | ')
             
             WL = [0] * 128; WLB = [0] * 128
             for i in range(16):
-                WL[i] = 1
+                WLB[i] = 1
                 dout = chip.cim(mmap=0xa, WL=WL, WLB=WLB)
-                print( hex(dout), end=' ' )
+                dout = dout & 1
+                print( dout, end=' ' )
             print ()
 
 for i in range(16 + 1):
     ret = chip.read_cam(0xa, i)
-    if ret != 0xf:
-        print ('Read Disturb! (%x)' % (ret))
+    if ret != 0x1:
+        print ('Read Disturb!')
