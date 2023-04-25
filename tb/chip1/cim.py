4f32672cbd0480c77be71a0b2bc1eb5f68b8b0bb
Brian Crafton <bcrafton3@gatech.edu>
2023-04-25 08:38:45 -0400

    clean up cim.py

--- tb/chip1/cim.py
+++ tb/chip1/cim.py
@@ -16,33 +16,27 @@ chip.rst()
 
 board.set_voltage('vbl', 300)
 board.set_voltage('avdd_wl', 525)
+board.set_voltage('vb1', 450)
+board.set_voltage('vb0', 400)
 
-chip.write_reg(reg=9, addr=0, val=0xff)
-chip.write_reg(reg=9, addr=1, val=0xff)
-chip.write_reg(reg=9, addr=2, val=0xff)
-chip.write_reg(reg=9, addr=3, val=0xff)
-chip.write_reg(reg=9, addr=4, val=0xff)
-chip.write_reg(reg=9, addr=5, val=0xff)
-
+# write 0x0f to 16 WLs
 for i in range(16 + 1):
     chip.write_cam(tgt, i, 0x0f, mux=mux)
 
-for vb1 in [450]:
-    board.set_voltage('vb1', vb1)
-    for vb0 in [400]:
-        board.set_voltage('vb0', vb0)
-        for vref in range(0, 200, 5):
-            board.set_voltage('vref', vref)
-            print (vb1, vb0, vref, end=' | ')
+# sweep vref=[0, 200] and WL=[1, 16]
+for vref in range(0, 200, 5):
+    board.set_voltage('vref', vref)
+    print (vref, end=' | ')
 
-            WL = [0] * 128; WLB = [0] * 128
-            for i in range(16):
-                WL[i] = 1
-                dout = chip.cim(mmap=0xa, WL=WL, WLB=WLB, mux=mux)
-                dout = dout & 1
-                print( dout, end=' ' )
-            print ()
+    WL = [0] * 128; WLB = [0] * 128
+    for i in range(16):
+        WL[i] = 1
+        dout = chip.cim(mmap=0xa, WL=WL, WLB=WLB, mux=mux)
+        dout = dout & 1
+        print( dout, end=' ' )
+    print ()
 
+# verify no read disturb occured on 16 WLs
 for i in range(16 + 1):
     ret = chip.read_cam(tgt, i, mux=mux)
     if ret != 0xf:
