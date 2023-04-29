36518915726a76c9625440fc2e942588acc6dd4e
Brian Crafton <bcrafton3@gatech.edu>
2023-04-29 09:42:34 -0400

    c stuff

---
+++ c/monitor2.py
@@ -0,0 +1,17 @@
+import numpy as np
+import serial
+import time
+from serial import Serial
+
+###############################################################
+
+# serial.serialutil.SerialException: [Errno 13] could not open port /dev/ttyACM0: [Errno 13] Permission denied: '/dev/ttyACM0'
+# sudo chmod 666 /dev/ttyACM0
+
+ser = Serial("/dev/ttyACM0", 9600)
+time.sleep(1)
+
+###############################################################
+
+ret = ser.write(bytes('wr', 'utf-8'))
+ret = ser.readline().decode("utf-8").strip()
